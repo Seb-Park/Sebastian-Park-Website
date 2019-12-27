7129e15a68bd5bebb073bd1c5acad4939de7fb34
Sebastian Park <SebPark03@gmail.com>
2019-12-27 00:17:02 -0500

    Update contactform.php

--- contact/contactform.php
+++ contact/contactform.php
@@ -15,7 +15,7 @@ if(isset($_POST['submit'])){
     echo("<h1>Form sent!</h1> \n Thanks for your feedback!");
 }
 else{
-    echo("<h1>Order 66 Error</h1>\n Form not sent...");
+    echo("<h1>Order 66 Executed Error</h1>\n Form not sent...");
 }
 echo("\n php WORKS!");
 
