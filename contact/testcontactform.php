3693d41c7b599ea6a52c59da559358743fc40e42
Sebastian Park <SebPark03@gmail.com>
2019-12-27 16:00:57 -0500

    Attempt to fix contact form floating issue.

--- contact/testcontactform.php
+++ contact/testcontactform.php
@@ -1,6 +1,6 @@
-<?PHP
+<!-- <?PHP
 $sender = 'sebpark03@gmail.com';
-$recipient = 'sebpark03@yahoo.com';
+$recipient = 'd9808952@urhen.com';
 
 $subject = "php mail test";
 $message = "php test message";
@@ -8,10 +8,27 @@ $headers = 'From:' . $sender;
 
 if (mail($recipient, $subject, $message, $headers))
 {
-    echo "Message accepted\n";
+    echo "Message accepted\n and sent to ".$recipient." from " .$sender;
 }
 else
 {
     echo "Error: Message not accepted";
 }
-?>
+?> -->
+
+<?php
+$to      = 'd9808952@urhen.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    if (mail($to, $subject, $message, $headers))
+    {
+        echo "Message accepted\n and sent to ".$recipient." from " .$sender;
+    }
+    else
+    {
+        echo "Error: Message not accepted";
+    }?>
