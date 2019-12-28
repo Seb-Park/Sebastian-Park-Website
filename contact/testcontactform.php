af01c4e0657e031beee4ac6bfe672cdcfa980d90
Sebastian Park <SebPark03@gmail.com>
2019-12-28 00:41:41 -0500

    Update testcontactform.php

--- contact/testcontactform.php
+++ contact/testcontactform.php
@@ -11,50 +11,50 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 // Instantiation and passing `true` enables exceptions
-if(isset($_POST['submit'])){
+// if($_POST['submit']){
 
     $name = $_POST['name'];
     $emailFrom = $_POST['email'];
     $message = $_POST['message'];
 
-$mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail -> Host = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'contactsparksoft@gmail.com';                     // SMTP username
-    $mail->Password   = "asdfghjkl;'";                               // SMTP password
-    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->SMTPSecure = 'ssl';
-    // $mail->Port       = 587;                                    // TCP port to connect to
-    $mail->Port = '465';
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail -> Host = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'contactsparksoft@gmail.com';                     // SMTP username
+        $mail->Password   = "asdfghjkl;'";                               // SMTP password
+        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->SMTPSecure = 'ssl';
+        // $mail->Port       = 587;                                    // TCP port to connect to
+        $mail->Port = '465';
 
-    //Recipients
-    $mail->setFrom('from@gmail.com', 'David_Ball@milton.edu');
-    $mail->addAddress('sparksoftgame@gmail.com');     // Add a recipient
-    // $mail->addAddress('ellen@example.com','Ellen Example');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+        //Recipients
+        $mail->setFrom('from@gmail.com', 'David_Ball@milton.edu');
+        $mail->addAddress('sparksoftgame@gmail.com');     // Add a recipient
+        // $mail->addAddress('ellen@example.com','Ellen Example');               // Name is optional
+        $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
 
-    // Attachments
+        // Attachments
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-}
-else{
-    echo('Message not sent...');
-}
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+// }
+// else{
+    // echo('Message not sent...');
+// }
 ?>
