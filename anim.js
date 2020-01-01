0754e9aeef3412f5c687f47b650785a2ce3f893c
Sebastian Park <SebPark03@gmail.com>
2019-12-31 19:27:57 -0500

    Testing set scene.

--- anim.js
+++ anim.js
@@ -1,7 +1,7 @@
 fadeOutFader();
 // document.body.style.backgroundColor = "lightblue";
 scrollFunction();
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 // alert("working!");
 
@@ -13,44 +13,70 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     var navigatorTopBar = document.getElementById("top").style;
     var slightlyMoving = document.getElementsByClassName("slightly-moving");
-  if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
-    navigatorTopBar.padding = "0px 20px";
-    navigatorTopBar.backgroundColor = "rgba(255, 105, 180, 0.89)";
-    navigatorTopBar.boxShadow = "0px 0px 15px rgb(51, 51, 51)";
-    // document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    navigatorTopBar.padding = "20px 20px";
-    navigatorTopBar.backgroundColor = "rgba(255, 105, 180, 0)";
-    navigatorTopBar.boxShadow = "none";
-    // document.getElementById("logo").style.fontSize = "35px";
-  }
-  for(var i = 0; i < slightlyMoving.length; i++){
-    //   console.log(document.documentElement.scrollTop);
-    // console.log(document.documentElement.scrollTop + "px");
-    // slightlyMoving[i].style.backgroundPositionY = (document.documentElement.scrollTop*-.7) + "px";//if background-position is fixed
-    slightlyMoving[i].style.backgroundPositionY = (document.documentElement.scrollTop*-.3) + "px";//if background-position is local
-  }
-//   console.log("Scrolled");
-  
+    if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+        navigatorTopBar.padding = "0px 20px";
+        navigatorTopBar.backgroundColor = "rgba(255, 105, 180, 0.89)";
+        navigatorTopBar.boxShadow = "0px 0px 15px rgb(51, 51, 51)";
+        // document.getElementById("logo").style.fontSize = "25px";
+    } else {
+        navigatorTopBar.padding = "20px 20px";
+        navigatorTopBar.backgroundColor = "rgba(255, 105, 180, 0)";
+        navigatorTopBar.boxShadow = "none";
+        // document.getElementById("logo").style.fontSize = "35px";
+    }
+    for (var i = 0; i < slightlyMoving.length; i++) {
+        //   console.log(document.documentElement.scrollTop);
+        // console.log(document.documentElement.scrollTop + "px");
+        // slightlyMoving[i].style.backgroundPositionY = (document.documentElement.scrollTop*-.7) + "px";//if background-position is fixed
+        slightlyMoving[i].style.backgroundPositionY = (document.documentElement.scrollTop * -.3) + "px";//if background-position is local
+    }
+    //   console.log("Scrolled");
+
 }
 
 // alert("working!");
 
 
-document.getElementById("home-button").addEventListener("click",function(){fadeInFader("../..")});
-document.getElementById("programming-button").addEventListener("click",function(){fadeInFader("../../programming")});
-document.getElementById("music-button").addEventListener("click",function(){fadeInFader("../../music")});
-document.getElementById("contact-button").addEventListener("click",function(){fadeInFader("../../contact")});
+document.getElementById("home-button").addEventListener("click", function () { setScene(0) });
+document.getElementById("programming-button").addEventListener("click", function () { fadeInFader("../../programming") });
+document.getElementById("music-button").addEventListener("click", function () { fadeInFader("../../music") });
+document.getElementById("contact-button").addEventListener("click", function () { fadeInFader("../../contact") });
 
-function fadeOutFader(){
+function fadeOutFader() {
     // alert("fading in fader!");
     fader = document.getElementById("fader");
     fader.style.opacity = "0";
 }
 
-function fadeInFader(url){
+function fadeInFader(url) {
     // alert("fading in fader!");
     fader = document.getElementById("fader");
     fader.style.opacity = "1";
-    setTimeout(function() {window.location.replace(url)}, 500)
+    setTimeout(function () { window.location.replace(url) }, 500)
+}
+
+function setScene(sceneNumber){
+    if(sceneNumber == 0){
+        document.documentElement.innerHTML = readTextFile("index.html");
+        console.log(readTextFile("index.html"));
+    }
+    if(sceneNumber == 1){
+        document.documentElement.innerHTML = readTextFile("../programming/index.html");
+    }
+    if(sceneNumber == 2){
+        document.documentElement.innerHTML = readTextFile("../music/index.html");
+    }
+    if(sceneNumber == 3){
+        document.documentElement.innerHTML = readTextFile("contact/index.html");
+        console.log(readTextFile("contact/index.html"));
+    }
+}
+
+function readTextFile(file) {
+    fetch(file)
+        .then(response => response.text())
+        .then((data) => {
+            console.log(""+data + "");
+            return data;
+        })
 }
