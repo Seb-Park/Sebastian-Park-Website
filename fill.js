3afce16ad5e3c0490619cd05fcd8beb574441d34
Sebastian Park <SebPark03@gmail.com>
2020-05-15 00:33:19 -0400

    Update fill.js to use only one function that takes in the item and the row so that there's no extra code to fill up the two rows separately.

--- fill.js
+++ fill.js
@@ -2,18 +2,12 @@ var projectPanel;
 
 $.getJSON("projects.json", function (json) {
     projectPanel = json;
-    projectPanel['row1'].forEach(fillRow1);
-    projectPanel['row2'].forEach(fillRow2);
+    projectPanel['row1'].forEach(element => fillProjectRow(element,"1"));
+    projectPanel['row2'].forEach(element => fillProjectRow(element,"2"));
 });
 
-function fillRow1(item) {
-    document.getElementById("proj-row-1").innerHTML +=
-        '<a class="pc-container" href="' + item['url'] + '" target="_blank">' +
-        '<img class="project-card"src="' + item['image'] + '"alt="">' +
-        '</a>';
-}
-function fillRow2(item) {
-    document.getElementById("proj-row-2").innerHTML +=
+function fillProjectRow(item,rowNo) {
+    document.getElementById("proj-row-"+rowNo).innerHTML +=
         '<a class="pc-container" href="' + item['url'] + '" target="_blank">' +
         '<img class="project-card"src="' + item['image'] + '"alt="">' +
         '</a>';
