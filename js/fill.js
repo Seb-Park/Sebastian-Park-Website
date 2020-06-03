7cf60847d96f741339a46a6c70e44367b1c80e35
Sebastian Park <SebPark03@gmail.com>
2020-06-03 14:21:40 -0400

    Change project cards to be background images instead of images and make the images children of the div so that the cards can be the right size. Also, change the animation so that the image scales past the card when hovered. Edit box shadows.

--- js/fill.js
+++ js/fill.js
@@ -2,13 +2,15 @@ var projectPanel;
 
 $.getJSON("projects.json", function (json) {
     projectPanel = json;
-    projectPanel['row1'].forEach(element => fillProjectRow(element,"1"));
-    projectPanel['row2'].forEach(element => fillProjectRow(element,"2"));
+    projectPanel['row1'].forEach(element => fillProjectRow(element, "1"));
+    projectPanel['row2'].forEach(element => fillProjectRow(element, "2"));
 });
 
-function fillProjectRow(item,rowNo) {
-    document.getElementById("proj-row-"+rowNo).innerHTML +=
+function fillProjectRow(item, rowNo) {
+    document.getElementById("proj-row-" + rowNo).innerHTML +=
         '<a class="pc-container" href="' + item['url'] + '" target="_blank">' +
-        '<img class="project-card"src="' + item['image'] + '"alt="">' +
+        `<div class = "project-card"  style="background-image: url(${item['image']});">` +
+        `<img src="${item['image']}" alt="" style="visibility:hidden; height: 170px";>` +
+        `</div>` +
         '</a>';
 }
