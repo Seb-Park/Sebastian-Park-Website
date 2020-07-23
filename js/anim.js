62d33998146425e5ef2016f4c78626de9e68393c
Sebastian Park <SebPark03@gmail.com>
2020-07-23 18:27:29 -0400

    Make coloring more responsive through css variables.

--- js/anim.js
+++ js/anim.js
@@ -4,6 +4,7 @@ var footer = document.getElementsByClassName("page-footer")[0];
 var navigatorTopBar = document.getElementById("top").style;
 var slightlyMoving = document.getElementsByClassName("slightly-moving");
 var dropdown = document.getElementsByTagName("nav")[0].style;
+var docStyle = document.documentElement.style;
 
 fadeOutFader();
 scrollFunction();
@@ -14,13 +15,15 @@ function scrollFunction() {
     if (document.body.scrollTop / document.body.clientHeight > 280 / 790
         || document.documentElement.scrollTop / document.body.clientHeight > 280 / 790) {
         navigatorTopBar.padding = "0px 20px";
-        navigatorTopBar.backgroundColor = "var(--nav-bg)";
-        dropdown.backgroundColor = "var(--nav-bg)";
+        docStyle.setProperty("--nav-bg", "rgba(255, 105, 180, 0.89)");
+        // navigatorTopBar.backgroundColor = "var(--nav-bg)";
+        // dropdown.backgroundColor = "var(--nav-bg)";
         navigatorTopBar.boxShadow = "0px 0px 10px rgb(51, 51, 51)";
     } else {
         navigatorTopBar.padding = "20px 20px";
-        navigatorTopBar.backgroundColor = "rgba(255, 105, 180, 0)";
-        dropdown.backgroundColor = "rgba(255, 105, 180, 0)";
+        docStyle.setProperty("--nav-bg", "rgba(255, 105, 180, 0)");
+        // navigatorTopBar.backgroundColor = "rgba(255, 105, 180, 0)";
+        // dropdown.backgroundColor = "rgba(255, 105, 180, 0)";
         navigatorTopBar.boxShadow = "none";
     }
     for (var i = 0; i < slightlyMoving.length; i++) {
